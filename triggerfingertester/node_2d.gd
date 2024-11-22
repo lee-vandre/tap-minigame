9b469b77a3f59c09a5891de2d6d370afe0c70ea2
Lee-Vandre Napier <lnapier023@student.wethinkcode.co.za>
2024-11-22 07:55:00 +0200

    added high score display,not updated automatically yet

---
+++ triggerfingertester/node_2d.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready
+var highscorelabel =$"../highscore"
+
+func _ready() -> void:
+	display_high_score()
+	
+
+
+func display_high_score()-> void:
+	var file =FileAccess.open("res://scores.txt",FileAccess.READ)
+	var line = file.get_line()
+	var splitline =line.split(",")
+	var name = splitline[0]
+	var score = splitline[1].to_int()/5
+	highscorelabel.text="High Score "+ splitline[1]+ " held by : "+name + " with "+ str(score)+" taps per second"
+	
